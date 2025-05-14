8322902cf7c700f8f30ebb4d5a501339259c94d5
Meya Warrior <meyawarrior@gmail.com>
2025-05-14 07:18:28 -0500

    adding things

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(modal__close-btn);
+
+editProfileBtn.addEventListener("click", function () {
+   editProfileModal.classList.add("modal_is-opened");
+});
+
+
+
