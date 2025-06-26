b944b0937d49893ab24ab2c8c69188f18f11254f
Meya Warrior <meyawarrior@gmail.com>
2025-06-26 07:30:53 -0500

    adding things

---
+++ scripts/validation.js
@@ -0,0 +1,37 @@
+const showInputError = (formEl, inputEl, errorMsg) => {
+    const errorMsgID = inputEl.id + "-error";
+    const errorMsgEl = document.querySelector("#" + errorMsgID);
+    errorMsgEl.textContent = errorMsg;
+};
+
+const checkInputValidity = (formEl, inputEl) => {
+    if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+    }
+};
+
+const setEventListeners = (formEl) => {
+    const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+    const buttonElement = formEl.querySelector(".modal__button");
+
+    //console.log(inputList);
+    //console.log(buttonElement);
+
+    toggleButtonState(inputList, buttonElement);
+
+    inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+    checkInputValidity(formEl, inputElement);
+   // toggleButtonState(inputList, buttonElement);
+    });
+    });
+};
+
+const enableValidation =() => {
+    const formList = document.querySelectorAll(".modal__form");
+    formList.forEach((formEl) => {
+    setEventListeners(formEl);
+    });
+};
+
+enableValidation();
