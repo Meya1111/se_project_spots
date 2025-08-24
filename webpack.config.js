3e4b76c00703c596f08302a55bbe4f148048f07c
Meya Warrior <meyawarrior@gmail.com>
2025-08-24 13:59:15 -0500

    correcting errors

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');       
 
 module.exports = {                                             
-  entry: './src/index.js',                                      
+  entry: './src/pages/index.js',                                      
 
   plugins: [                                                    
     new HtmlWebpackPlugin({                                    
